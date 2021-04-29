189e28503ebb86f879123741720bceb0982862c8
Aaron McCauley <aaron.mccauley@salesforce.com>
2021-04-28 17:36:52 -0700

    Updated basic layout

--- force-app/main/default/lwc/conversationMap/conversationMap.js
+++ force-app/main/default/lwc/conversationMap/conversationMap.js
@@ -1,4 +1,18 @@
 import { LightningElement } from 'lwc';
 
 //testing!!
-export default class ConversationMap extends LightningElement {}
+export default class ConversationMap extends LightningElement {
+activeSections = ['A', 'C'];
+    activeSectionsMessage = '';
+
+    handleSectionToggle(event) {
+        const openSections = event.detail.openSections;
+
+        if (openSections.length === 0) {
+            this.activeSectionsMessage = 'All sections are closed';
+        } else {
+            this.activeSectionsMessage =
+                'Open sections: ' + openSections.join(', ');
+        }
+    }
+}
